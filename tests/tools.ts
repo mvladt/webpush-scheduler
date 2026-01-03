37a956956d0c44fd9f4bf9cfb41b7e6a705ab594
mvladt <v-mell@yandex.ru>
2026-01-03 14:51:10 +0300

    Добавил тулзу для создания тестового app для e2e

--- tests/tools.ts
+++ tests/tools.ts
@@ -1,3 +1,12 @@
+import { access, rm } from "node:fs/promises";
+import webpush from "web-push";
+
+import { createApp } from "../src/app.ts";
+import { createRouter } from "../src/router/router.ts";
+import { createNotificationScheduler } from "../src/scheduler/scheduler.ts";
+import { createJsonStore } from "../src/jsonStore/store.ts";
+import { createWebPusher } from "../src/pusher/pusher.ts";
+
 import type { NotificationEntity } from "../src/types.ts";
 
 export const createFakeNotification = (
@@ -13,3 +22,35 @@ export const createFakeNotification = (
 
 export const dumbUUID = (): string =>
   `id-${Date.now().toString(16)}-${Math.random().toString(16).slice(2)}`;
+
+export const cleanupTestFile = async (filePath: string): Promise<void> => {
+  try {
+    await access(filePath);
+    await rm(filePath);
+  } catch (error: any) {
+    if (error.code !== "ENOENT") {
+      throw error; // Кидаем только если не "file not found"
+    }
+  }
+};
+
+export const createTestApp = () => {
+  const testFile = `test-notifications-${dumbUUID()}.json`;
+  const vapidKeys = webpush.generateVAPIDKeys();
+
+  const store = createJsonStore(testFile);
+  const pusher = createWebPusher({
+    vapidDetails: {
+      subject: "mailto:test@example.com",
+      publicKey: vapidKeys.publicKey,
+      privateKey: vapidKeys.privateKey,
+    },
+  });
+  const scheduler = createNotificationScheduler(store, pusher, {
+    intervalMs: 2000,
+  });
+  const router = createRouter(scheduler, pusher);
+  const app = createApp(0, router, scheduler);
+
+  return { app, testFile, vapidKeys };
+};
